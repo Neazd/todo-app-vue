1e42aa641daf6b724626beb458d6fb21ab22463f
johnyrlk <jan.rycielski@gmail.com>
2024-07-24 21:48:33 +0200

    add edit option nad css

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="app">
-    <h1>Task List</h1>
-    <div>
-      <form @submit.prevent="submitForm">
-        <input type="text" v-model="newTask" placeholder="Enter a task" />
-        <button type="submit">Add Task</button>
+  <div id="app" class="app-container">
+    <h1 class="app-title">Task List</h1>
+    <div class="form-container">
+      <form @submit.prevent="submitForm" class="task-form">
+        <input type="text" v-model="newTask" placeholder="Enter a task" class="task-input"/>
+        <button type="submit" class="task-button small">Add Task</button>
       </form>
     </div>
-    <TasksComponent :items="tasks" @update-items="updateItems" @delete-item="deleteTask"/>
+    <TasksComponent :items="tasks" @update-item="updateTask" @delete-item="deleteTask"/>
   </div>
 </template>
 
@@ -27,13 +27,16 @@ export default {
   },
   methods: {
     submitForm() {
-      if (this.newTask) {
-        this.tasks.push(this.newTask);
+      if (this.newTask.trim()) {
+        this.tasks.push({ text: this.newTask, editing: false });
         this.newTask = "";
       }
     },
-    updateItems(newTasks) {
-      this.tasks = newTasks;
+    updateTask(updatedItem) {
+      const index = this.tasks.findIndex(task => task.text === updatedItem.originalText);
+      if (index !== -1) {
+        this.tasks[index].text = updatedItem.newText;
+      }
     },
     deleteTask(index) {
       this.tasks.splice(index, 1);
@@ -43,5 +46,51 @@ export default {
 </script>
 
 <style>
-/* Add your styles here */
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+.app-title {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+.form-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+.task-form {
+  display: flex;
+  gap: 10px;
+}
+
+.task-input {
+  padding: 10px;
+  font-size: 1em;
+  width: 300px;
+}
+
+.task-button {
+  padding: 10px 20px;
+  font-size: 1em;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+.task-button.small {
+  padding: 5px 10px;
+  font-size: 0.8em;
+}
+
+.task-button:hover {
+  background-color: #0056b3;
+}
 </style>
--- src/components/TasksComponent.vue
+++ src/components/TasksComponent.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>
-    <ul>
-      <li  v-for="(item, index) in items" :key="index"> <input v-model="isChecked" type="checkbox"/> <div v-if="!item.editing" :class="{'strikethrough': isChecked}">{{ item }} </div><button @click="updateTask(index)">Update Task</button><button @click="deleteItem(index)">Delete</button></li>
+  <div class="tasks-container">
+    <ul class="tasks-list">
+      <li v-for="(item, index) in items" :key="index" class="task-item">
+        <input v-model="isChecked" type="checkbox" class="task-checkbox"/>
+        <div v-if="!item.editing" :class="{'strikethrough': isChecked}" class="task-text">
+          {{ item.text }}
+        </div>
+        <input v-if="item.editing" v-model="item.newText" @keyup.enter="finishEditing(item, index)" class="task-input"/>
+        <div class="task-buttons">
+          <button v-if="!item.editing" @click="editTask(item)" class="task-button small">Edit</button>
+          <button v-if="item.editing" @click="finishEditing(item, index)" class="task-button small">Save</button>
+          <button @click="deleteItem(index)" class="task-button small">Delete</button>
+        </div>
+      </li>
     </ul>
   </div>
-  <h1>Zmienna jest:{{inputValue}}</h1>
 </template>
 
 <script>
@@ -16,24 +26,87 @@ export default {
       required: true
     }
   },
-  data(){
+  data() {
     return {
       isChecked: false,
-    }
+    };
   },
   methods: {
     deleteItem(index) {
       this.$emit('delete-item', index);
     },
-    updateTask(index){
-      this.$emit('update-item', index)
+    editTask(item) {
+      item.editing = true;
+      item.newText = item.text;
+    },
+    finishEditing(item) {
+      item.editing = false;
+      if (item.text !== item.newText) {
+        this.$emit('update-item', { newText: item.newText, originalText: item.text });
+      }
     }
   },
 };
 </script>
 
 <style>
+.tasks-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.tasks-list {
+  list-style-type: none;
+  padding: 0;
+  width: 100%;
+}
+
+.task-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+}
+
+.task-checkbox {
+  margin-right: 10px;
+}
+
+.task-text {
+  flex-grow: 1;
+}
+
 .strikethrough {
   text-decoration: line-through;
+}
+
+.task-input {
+  flex-grow: 1;
+  margin-right: 10px;
+}
+
+.task-buttons {
+  display: flex;
+  gap: 10px;
+}
+
+.task-button {
+  padding: 5px 5px;
+  margin: 10px;
+  border: none;
+  background-color: #007bff;
+  color: white;
+  cursor: pointer;
+}
+
+.task-button.small {
+  padding: 3px 8px;
+  font-size: 0.8em;
+}
+
+.task-button:hover {
+  background-color: #0056b3;
 }
 </style>
